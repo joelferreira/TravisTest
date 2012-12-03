ddab5d9942979a52dc6ab5321e21fdb792a78ebb
Joel Ferreira <joelfilipeferreira@gmail.com>
2012-12-03 16:31:42 +0000

    changed test

--- test.rb
+++ test.rb
@@ -1,1 +1,15 @@
-assert_equal(4, 2+2)
+class TestSimpleNumber < Test::Unit::TestCase
+ 
+  def setup
+    
+  end
+ 
+  def teardown
+    ## Nothing really
+  end
+ 
+  def test_simple
+    assert_equal(4, 2+2 )
+  end
+ 
+end
